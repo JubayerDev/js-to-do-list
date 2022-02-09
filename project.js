002f35da8c1f2a6e592e6e390a1ccb95b4a76ae4
JubayerDev <ajashes858@gmail.com>
2022-02-09 19:56:21 +0600

    done

--- project.js
+++ project.js
@@ -12,12 +12,35 @@ document.getElementById("add-btn").addEventListener('click', function () {
         tableContainer.innerHTML = `<th>${count}</th>
         <td>${inputValue}</td>
         <td>
-        <button class="btn btn-danger">Delete</button>
-        <button class="btn btn-success">Done</button>
+        <button class="btn btn-danger delete-btn">Delete</button>
+        <button class="btn btn-success done-btn">Done</button>
         </td>`;
 
         mainContainer.appendChild(tableContainer);
 
         document.getElementById("input-value").value = '';
+
+        const deleteButton = document.getElementsByClassName("delete-btn");
+        const doneButton = document.getElementsByClassName("done-btn");
+
+        for (const button of deleteButton) {
+            button.addEventListener('click', function (e) {
+                e.target.parentNode.parentNode.style.display = 'none';
+            })
+        }
+
+        for (button of doneButton) {
+            button.addEventListener('click', function (e) {
+                e.target.parentNode.parentNode.style.textDecoration = 'line-through';
+            })
+        }
+
+        // Clear ALL
+        const clearAll = document.getElementsByClassName('clear-btn');
+
+        for (const button of clearAll) {
+            button.addEventListener('click', function (e) {
+            })
+        }
     }
 })
